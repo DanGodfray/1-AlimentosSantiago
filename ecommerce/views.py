1dd86af71ad3622dd6a846146f0598ec2ae5e2d2
--list <d.godfray@duocuc.cl>
2024-06-17 04:17:41 -0400

    comit crud fallido

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -5,7 +5,7 @@ from django.core.paginator import Paginator
 from django.contrib import messages
 from usuarios.models import Proveedor
 
-banderaOferta = False
+
 
 # Create your views here.
 
@@ -18,10 +18,15 @@ def listarCatalogos(request):
     context = {"categorias":categoria, "platos":plato, "ofertas":oferta}
     return render(request, 'ecommerce/catalogos.html', context)
 
+banderaCatActivo = False
+
 def listarCategorias(request):
-    categoria = Categoria.objects.all()
-    context = {"arregloCat":categoria}
+    banderaCatActivo = True
+    categoria = Categoria.objects.filter(cat_activo=True).all()
+    context = {"arregloCat":categoria, "banderaCatActivo":banderaCatActivo}
     return render(request, 'ecommerce/categorias.html', context)
+
+banderaOferta = False
 
 def listarOfertas(request):
     banderaOferta = True
@@ -61,7 +66,8 @@ def registrarPlato(request):
         oferta = request.POST.get('oferta')
         categoria = request.POST.get('categoria')
         foto = request.FILES.get('foto')
-        
+        descuento = request.POST.get('descuento')
+        plato_activo = request.POST.get('plato_activo')
         #se obtiene el id de la categoria seleccionada
         categoria = Categoria.objects.get(nom_categoria=categoria)
         #se crea un nuevo plato
@@ -70,7 +76,8 @@ def registrarPlato(request):
                       descripcion_plato=descripcion, 
                       precio_plato=precio, 
                       oferta_plato=oferta, 
-                      foto_plato=foto)
+                      foto_plato=foto,
+                      descuento_activo=descuento, plato_activo=plato_activo)
         plato.save()
         messages.success(request, 'Plato registrado correctamente')
         return redirect('registroPlato')
--- usuarios/views.py
+++ usuarios/views.py
@@ -4,6 +4,7 @@ from ecommerce.models import Categoria, Plato
 from .models import Proveedor, Venta
 from django.core.paginator import Paginator
 from django.contrib import messages
+from datetime import date
 
 # Create your views here.
 
@@ -62,27 +63,65 @@ def pausarPlato(request):
     return render(request, 'usuarios/proveedor.html', context)
 
 def registrarPlato(request):
-    if request.method == 'POST':
-        nombre = request.POST.get('nombre')
-        descripcion = request.POST.get('descripcion')
-        precio = request.POST.get('precio')
-        oferta = request.POST.get('oferta')
-        categoria = request.POST.get('categoria')
-        foto = request.FILES.get('foto')
+
+    try:
+        if request.method != 'POST':
+            
+            print("no es post")
+            
+            plato = Plato.objects.all()
+            categoria = Categoria.objects.all()
+            proveedor = Proveedor.objects.all()
+ 
+            #se obtienen los datos del formulario si selecciona otra categoria   
+            context = {"listaPlatos": plato ,"listaCategorias":categoria, "listaProveedores":proveedor}
+            
+            print("se rednderio default")
+            return render(request, 'usuarios/plato_add.html', context)
+            
+        else:    
+            print("es post")    
+                 
+            categoria = request.POST.get('categoria')
+            nombre = request.POST.get('nombre')
+            descripcion = request.POST.get('descripcion')
+            precio = request.POST.get('precio')
+            oferta = request.POST.get('oferta') 
+            foto = request.FILES.get('foto')
+            proveedor = request.POST.get('proveedor')
+            #se registra la fecha de registro del plato
+            fecha = date.today()
+            descuento = request.POST.get('descuento')
+            plato_activo = request.POST.get('plato_activo')
+            proveedor = request.POST.get('proveedor')
+            
+            objetoProveedor = Proveedor.objects.get(id_proveedor=proveedor)
+            
+            #se obtiene el id de la categoria seleccionada
+            objetoCategoria = Categoria.objects.get(nom_categoria=categoria)
+            
+            #se crea un nuevo plato
+            objetoPlato = Plato(id_categoria=objetoCategoria, 
+                        nom_plato=nombre, 
+                        descripcion_plato=descripcion, 
+                        precio_plato=precio, 
+                        oferta_plato=oferta, 
+                        foto_plato=foto,
+                        fecha_publicacion=fecha,
+                        descuento_activo=descuento,
+                        plato_activo=plato_activo,
+                        proveedor=objetoProveedor)
+            
+            objetoPlato.save()
+            context = {"mensaje":"Plato registrado correctamente"}
+            messages.success(request, 'Plato registrado correctamente')
+            return render(request, 'usuarios/plato_add.html', context)
         
-        #se obtiene el id de la categoria seleccionada
-        categoria = Categoria.objects.get(nom_categoria=categoria)
-        #se crea un nuevo plato
-        plato = Plato(id_categoria=categoria, 
-                      nom_plato=nombre, 
-                      descripcion_plato=descripcion, 
-                      precio_plato=precio, 
-                      oferta_plato=oferta, 
-                      foto_plato=foto)
-        plato.save()
-        messages.success(request, 'Plato registrado correctamente')
-        return redirect('registroPlato')
-    else:
+    except Exception as e:
+        print(e)
+        plato = Plato.objects.all()
         categoria = Categoria.objects.all()
-        context = {"categorias":categoria}
-        return render(request, 'ecommerce/publicaciones.html', context)
+        proveedor = Proveedor.objects.all()
+        context = {"mensaje":"Error al registrar el plato", "listaPlatos": plato ,"listaCategorias":categoria, "listaProveedores":proveedor}
+        messages.error(request, 'Error al registrar el plato')
+        return render(request, 'usuarios/plato_add.html', context)
