81ddd12cdff6e70f4301416a1df2693a1ff9e533
--list <d.godfray@duocuc.cl>
2024-06-26 15:08:26 -0400

    actualizo el usuario repartidor y se agrega un .txt con credenciales de prueba

--- repartidor/models.py
+++ repartidor/models.py
@@ -1,18 +1,16 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 # Creación de la tabla Repartidor
 class Repartidor(models.Model):
     id_repartidor = models.AutoField(primary_key=True)
-    rut_repartidor = models.CharField(max_length=12, blank=False, null=False)
-    #nombre_repartidor = models.CharField(max_length=100, blank=False, null=False)
-    fono_repartidor = models.CharField(max_length=15, blank=False, null=False)
     
-    #credenciales de acceso
-    #apellido_repartidor = models.CharField(max_length=100, blank=True, null=True)
-    #email_repartidor = models.EmailField(max_length=100, blank=False, null=False)
-    #password_repartidor = models.CharField(max_length=100, blank=False, null=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    rut_repartidor = models.CharField(max_length=12, blank=False, null=False)
+    fono_repartidor = models.CharField(max_length=15, blank=False, null=False)
     def __str__(self):
-        return self.nombre_repartidor
+        return self.rut_repartidor
