d27cc07c8e4aaa48f2e6b16dc340d69683b2bee2
--list <d.godfray@duocuc.cl>
2024-11-21 23:51:13 -0300

    se modifica ALLOWED_HOSTS para probar despliegue

--- alimentoSantiago/settings.py
+++ alimentoSantiago/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-jmm&^e^b&r(mrx70^bh(uzv3ztsd=4*6mbyleogp=#me7nwp2w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
